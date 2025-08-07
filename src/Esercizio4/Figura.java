b6604997a9faadaae7321e1a01818e4381676da7
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-08-07 22:20:19 +0200

    correzione classe astratta

--- src/Esercizio4/Figura.java
+++ src/Esercizio4/Figura.java
@@ -1,22 +1,11 @@
 package Esercizio4;
 
-public class Figura {
+public abstract class Figura {
 //public final class Figura
 
     public static final double PI_GRECO = 3.14;
     protected double area;
 
-    public Figura(){
-    }
-
-    public double calcolaArea(){
-        return area;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-
+    public abstract double calcolaArea();
     
 }
